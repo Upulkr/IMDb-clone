7b4718d264998105faccc5c87b9aba19f37d729c
Upul <upulkr4@gmail.com>
2023-10-26 23:28:07 +0530

    finished

---
+++ src/app/loading.jsx
@@ -0,0 +1,9 @@
+function loading() {
+    return (
+        <div className="flex ">
+            <img  className='h-96 flex mx-auto items-center justify center'src='eclipse.svg' alt='loading...'/>
+        </div>
+    )
+}
+
+export default loading
---
+++ src/components/NavbarItem.jsx
@@ -0,0 +1,26 @@
+"use client";
+import Link from "next/link";
+import { useSearchParams } from "next/navigation";
+
+
+
+function NavbarItem({ title, param }) {
+  const searchParamas = useSearchParams();
+  const genre = searchParamas.get("genre");
+  return (
+    <div>
+    <Link
+    className={`m-4 hover:text-amber-600 font-semibold p-2 ${
+      genre &&
+      genre === param &&
+      "underline underline-offset-8 decoration-4  decoration-amber-500 rounded-lg"
+    }`}
+    href={`/?genre=${param}`}
+  >
+    {title}
+  </Link>
+    </div>
+  );
+}
+
+export default NavbarItem;
